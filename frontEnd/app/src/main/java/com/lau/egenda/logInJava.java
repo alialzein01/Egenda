59b238877f2b65d5ea7e405a77a3853973f6d0b9
Ali Alzein <aly.alzein01@gmail.com>
2022-04-12 08:08:56 +0300

    adding some intents

--- frontEnd/app/src/main/java/com/lau/egenda/logInJava.java
+++ frontEnd/app/src/main/java/com/lau/egenda/logInJava.java
@@ -2,7 +2,9 @@ package com.lau.egenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class logInJava extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class logInJava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+    }
+
+    public void signPage(View view) {
+        Intent i = new Intent(logInJava.this, signupJava.class);
+        startActivity(i);
     }
 }
--- frontEnd/app/src/main/java/com/lau/egenda/signupJava.java
+++ frontEnd/app/src/main/java/com/lau/egenda/signupJava.java
@@ -2,7 +2,9 @@ package com.lau.egenda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class signupJava extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class signupJava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+    }
+
+    public void goBack(View view) {
+        Intent i = new Intent(signupJava.this, logInJava.class);
+        startActivity(i);
     }
 }
