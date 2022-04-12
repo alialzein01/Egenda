926840748969579d28156022e539afe70ec847ed
Ali Alzein <aly.alzein01@gmail.com>
2022-04-12 07:21:04 +0300

    adding login and signup

---
+++ frontEnd/app/src/main/java/com/lau/egenda/signupJava.java
@@ -0,0 +1,14 @@
+package com.lau.egenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signupJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
