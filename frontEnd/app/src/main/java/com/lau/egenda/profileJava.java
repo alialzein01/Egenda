1fe8dbd5cc31187616a7acb213b838f09bd60c5f
Ali Alzein <aly.alzein01@gmail.com>
2022-04-13 07:22:26 +0300

    adding profile page

---
+++ frontEnd/app/src/main/java/com/lau/egenda/profileJava.java
@@ -0,0 +1,14 @@
+package com.lau.egenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class profileJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
