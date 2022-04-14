5c2853ef6a18fb34a93770e884186a87cca882af
Ali Alzein <aly.alzein01@gmail.com>
2022-04-14 11:44:10 +0300

    Adding course page

---
+++ frontEnd/app/src/main/java/com/lau/egenda/coursesJava.java
@@ -0,0 +1,14 @@
+package com.lau.egenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coursesJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.courses);
+    }
+}
