8ad43671055234afe80623220634454b2d3bd95c
Ali Alzein <aly.alzein01@gmail.com>
2022-04-18 05:06:36 +0300

    Adding assignments page

---
+++ frontEnd/app/src/main/java/com/lau/egenda/HwtrackJava.java
@@ -0,0 +1,14 @@
+package com.lau.egenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HwtrackJava extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hwtrack);
+    }
+}
