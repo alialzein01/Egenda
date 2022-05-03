e435c8bcdf4a7cdedd887a8a443b0a70441f556a
Ali Alzein <aly.alzein01@gmail.com>
2022-05-03 20:51:08 +0300

    adding signup php file

---
+++ backEnd/userregister.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    include 'DatabaseConfig.php'; // linking the database.
+
+    // Sending values to DB.
+    $S_fullname = $_POST['FullName'];
+    $S_username = $_POST['Username'];
+    $S_email = $_POST['Email'];
+    $S_password = $_POST['Password'];
+    $S_image = $_POST['Image'];
+    $S_Cid = $_POST['Cid'];
+
+    //SQL query to insert values into the user table.
+    $Sql_Query = "INSERT INTO user (FullName,Username,Email, Password,Image,Cid) values ('$S_fullname','$S_username', '$S_email','$S_password','$S_image','$S_Cid')";
+}
+mysqli_close($con);
