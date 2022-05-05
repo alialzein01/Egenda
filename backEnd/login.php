6d24b22f3410598f3b3a25dd29f38e340b220fc8
Ali Alzein <aly.alzein01@gmail.com>
2022-05-05 21:44:33 +0300

    adding DB file and login php file to backend

---
+++ backEnd/login.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    include('DatabaseConfig.php');
+    $con = mysqli_connect($HostName, $HostUser, $HostPass, $DatabaseName);
+    $S_username = $_POST['Username'];
+    $S_password = $_POST['Password'];
+
+    $Sql_Query = "Select * FROM User Where Username = '$S_username' && Password = '$S_password'";
+
+    if (mysqli_query($con, $Sql_Query)) {
+        echo 'login successfully';
+    } else {
+        echo 'Something went wrong';
+    }
+}
+mysqli_close($con);
